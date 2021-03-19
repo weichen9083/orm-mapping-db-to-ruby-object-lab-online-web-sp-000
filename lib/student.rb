33715a65ec1c642f051fc2d25631134f57a9c9ef
wei chen <weichen9083@gmail.com>
2021-03-19 02:30:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,6 +75,13 @@ def self.first_student_in_grade_10
   
 end 
 
+def self.all 
+  sql = "select * from students"
+  DB[:conn].execute(sql).map |row|
+  self.new_from_db(row)
+end 
+end 
+
   
   
   
