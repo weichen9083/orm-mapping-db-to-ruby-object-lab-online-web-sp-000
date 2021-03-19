2ce58557278f447558dcd733c8779e7d5434143e
wei chen <weichen9083@gmail.com>
2021-03-19 02:26:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,9 +59,18 @@ end
     
   end
   
-  def self..first_X_students_in_grade_10(row)
-    
-  end 
+def self.first_X_students_in_grade_10(x)
+  sql = <<-SQL
+  SELECT * FROM students WHERE grade = 10 LIMIT ?
+  SQL
+  
+  DB[:conn].execute(sql, x).map do |row|
+  self.new_from_db(row)
+end
+end 
+
+def self.
+
   
   
   
